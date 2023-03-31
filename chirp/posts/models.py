f9eaeef19aead3851e645f63fda47d512e80d24b
Rachel Lloyd <rachellloyd@Rachels-MacBook-Pro.local>
2023-03-30 20:15:58 -0400

    fixed issues

--- chirp/posts/models.py
+++ chirp/posts/models.py
@@ -7,8 +7,7 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True,)
     body = models.TextField(max_length=1000)
     title = models.CharField(max_length=100)
-    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True)
-    # avatar = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
+    photo = models.ImageField(upload_to='users/%Y/%m/%d', blank=True, null=True)
 
     def __str__(self):
         return self.body
